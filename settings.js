4683d30f4110eeca013b18055768154f2c903504
OLAOLEX <106306704+OLAOLEX@users.noreply.github.com>
2022-05-28 12:34:28 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['2348060800971']
+global.premium = ['2348060800971']
+global.ownernomer = '2348060800971'
+global.ownername = 'Olaolex'
+global.botname = 'Olaolex'
+global.footer = 'Olaolex'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.packname = 'Olaolex'
+global.author = 'Olaolex'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
